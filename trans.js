fd12cc08b59af250a4974ecd661550751a12c9c6
anhpvdev <105933869+anhpvdev@users.noreply.github.com>
2022-07-04 20:28:50 +0700

    Add files via upload

---
+++ trans.js
@@ -0,0 +1,10 @@
+
+function showcommunity(){
+    document.getElementById("community").style.display = "block";
+    console.log("done");
+}
+
+function hide(){
+    document.getElementById("community").style.display = "none";
+    console.log("faid");
+}
